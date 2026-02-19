6f9390f3b48e32dd8d98df563587c3a12f093105
ulan07 <122809742+ulan07@users.noreply.github.com>
2026-02-19 21:37:48 +0500

    Add files via upload

---
+++ 2 w/1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string a;
+    cin>>a;
+    for(int i=0;i<2;i++){
+        cout<<a;
+    } 
+    cout<<"change smth";
+    return 0;
+}
---
+++ 2 w/4.py
@@ -0,0 +1,3 @@
+is_logged_in = True
+if is_logged_in:
+  print("Welcome back!")
